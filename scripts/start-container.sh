e4534a7b9c6f56ceb2980b556d57c3831c6bda2d
saschazesiger <janis.zesiger@edu.tbz.ch>
2023-04-28 00:23:44 +0200

    fix

--- scripts/start-container.sh
+++ scripts/start-container.sh
@@ -1,6 +1,6 @@
 cd ${DATA_DIR}
 wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/firefox-112.0.2.tar.bz2 "https://download-installer.cdn.mozilla.net/pub/firefox/releases/112.0.2/linux-x86_64/en-US/firefox-112.0.2.tar.bz2"
-tar tar xjf firefox-*.tar.bz2
+tar xjf firefox-*.tar.bz2
 ls
 
 rm -f ${DATA_DIR}/firefox-112.0.2.tar.bz2

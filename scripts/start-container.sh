2d8d6713a413aca829defd1ad399155111448228
saschazesiger <janis.zesiger@edu.tbz.ch>
2023-04-28 00:08:08 +0200

    other update

--- scripts/start-container.sh
+++ scripts/start-container.sh
@@ -1,7 +1,7 @@
 cd /firefox
-wget -q -nc --show-progress --progress=bar:force:noscroll -O firefox-112.0.2.tar.bz2 "https://download-installer.cdn.mozilla.net/pub/firefox/releases/112.0.2/linux-x86_64/en-US/firefox-112.0.2.tar.bz2"
+wget -q -nc --show-progress --progress=bar:force:noscroll -O /firefox/firefox-112.0.2.tar.bz2 "https://download-installer.cdn.mozilla.net/pub/firefox/releases/112.0.2/linux-x86_64/en-US/firefox-112.0.2.tar.bz2"
 ls
-tar -C /firefox --strip-components=2 -xf firefox-112.0.2.tar.bz2
+tar -C /firefox --strip-components=2 -xf /firefox/firefox-112.0.2.tar.bz2
 ls
-rm -f firefox-112.0.2.tar.bz2
+rm -f /firefox/firefox-112.0.2.tar.bz2
 rm -rf /tmp/profile
